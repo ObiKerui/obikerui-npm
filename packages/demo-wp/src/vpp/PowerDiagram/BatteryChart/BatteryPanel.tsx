43121608e7cfb42ccee18b76355d8a00dc9a939d
Craig Sharp <craigsharp@craigs-mbp.home>
2024-08-23 20:15:20 +0100

    commented out battery classes not ready

--- packages/demo-wp/src/vpp/PowerDiagram/BatteryChart/BatteryPanel.tsx
+++ packages/demo-wp/src/vpp/PowerDiagram/BatteryChart/BatteryPanel.tsx
@@ -1,52 +1,52 @@
-import { useEffect, useRef } from 'react';
-import { usePowerRouter } from '../../Solax/Store';
-import { useBatteryChart } from './Chart';
-import { DataGrouper } from '../../Solax/Grouper';
+// import { useEffect, useRef } from 'react';
+// import { usePowerRouter } from '../../Solax/Store';
+// import { useBatteryChart } from './Chart';
+// import { DataGrouper } from '../../Solax/Grouper';
 
-const grouper = new DataGrouper();
+// const grouper = new DataGrouper();
 
-function BatteryPanel() {
-  const { data } = usePowerRouter();
-  const { setContainer, setGroupedData, setCategories, timeFrame } =
-    useBatteryChart();
-  const chartRef = useRef<HTMLDivElement | null>(null);
+// function BatteryPanel() {
+//   const { data } = usePowerRouter();
+//   const { setContainer, setGroupedData, setCategories, timeFrame } =
+//     useBatteryChart();
+//   const chartRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    setContainer(chartRef.current);
-  }, []);
+//   useEffect(() => {
+//     setContainer(chartRef.current);
+//   }, []);
 
-  useEffect(() => {
-    let groupedData = null;
-    switch (timeFrame) {
-      case 'days':
-        groupedData = grouper.groupByDays(data);
-        break;
-      case 'hours':
-        groupedData = grouper.groupByHours(data);
-        break;
-      case 'weeks':
-        groupedData = grouper.groupByWeeks(data);
-        break;
-      case 'months':
-        groupedData = grouper.groupByMonths(data);
-        break;
-      default:
-        break;
-    }
-    if (!groupedData) return;
+//   useEffect(() => {
+//     let groupedData = null;
+//     switch (timeFrame) {
+//       case 'days':
+//         groupedData = grouper.groupByDays(data);
+//         break;
+//       case 'hours':
+//         groupedData = grouper.groupByHours(data);
+//         break;
+//       case 'weeks':
+//         groupedData = grouper.groupByWeeks(data);
+//         break;
+//       case 'months':
+//         groupedData = grouper.groupByMonths(data);
+//         break;
+//       default:
+//         break;
+//     }
+//     if (!groupedData) return;
 
-    setGroupedData(groupedData);
-    setCategories(['soc']);
-  }, [timeFrame, data]);
+//     setGroupedData(groupedData);
+//     setCategories(['soc']);
+//   }, [timeFrame, data]);
 
-  return (
-    <div>
-      {/* <div>
-        <Controls />
-      </div> */}
-      <div ref={chartRef} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {/* <div>
+//         <Controls />
+//       </div> */}
+//       <div ref={chartRef} />
+//     </div>
+//   );
+// }
 
-export { BatteryPanel };
+// export { BatteryPanel };
--- packages/demo-wp/src/vpp/PowerDiagram/BatteryChart/PercentChart.ts
+++ packages/demo-wp/src/vpp/PowerDiagram/BatteryChart/PercentChart.ts
@@ -1,85 +1,82 @@
-import * as d3PlotLib from '@obikerui/d3-plot-lib';
-import * as d3 from 'd3';
-import { create } from 'zustand';
-import dayjs from 'dayjs';
-import { tPowerCategory } from '../../Solax/Types';
+// import * as d3PlotLib from '@obikerui/d3-plot-lib';
+// import * as d3 from 'd3';
+// import { create } from 'zustand';
+// import dayjs from 'dayjs';
+// import { tPowerCategory } from '../../Solax/Types';
 
-class Chart {
-  container;
-  constructor() {
-    this.container = new d3PlotLib.CContainer();
-    this.container.addPlot(new d3PlotLib.CBar());
-  }
+// class Chart {
+//   container;
+//   constructor() {
+//     this.container = new d3PlotLib.CContainer();
+//     this.container.addPlot(new d3PlotLib.CBar());
+//   }
 
-  //   getMinMax(category: tPowerCategory) {
-  //     switch (category) {
-  //       case 'consumption':
-  //         return [0, 5000];
-  //       case 'dailyYield':
-  //         return [0, 400];
-  //       case 'feedInEnergy':
-  //         return [0, 5000];
-  //       case 'soc':
-  //         return [0, 100];
-  //       default:
-  //         return [0, 0];
-  //     }
-  //   }
+//   //   getMinMax(category: tPowerCategory) {
+//   //     switch (category) {
+//   //       case 'consumption':
+//   //         return [0, 5000];
+//   //       case 'dailyYield':
+//   //         return [0, 400];
+//   //       case 'feedInEnergy':
+//   //         return [0, 5000];
+//   //       case 'soc':
+//   //         return [0, 100];
+//   //       default:
+//   //         return [0, 0];
+//   //     }
+//   //   }
 
-  update(newModel: tBatteryChart) {
-    const { container, categories, rangedData } = newModel;
+//   update(newModel: tBatteryChart) {
+//     const { container, categories, rangedData } = newModel;
 
-    if (!rangedData) {
-      return;
-    }
+//     if (!rangedData) {
+//       return;
+//     }
 
-    // const extent = d3.extent(
-    //   rangedData,
-    //   (elem) => dayjs(elem.uploadTime).toDate() as Date
-    // );
+//     // const extent = d3.extent(
+//     //   rangedData,
+//     //   (elem) => dayjs(elem.uploadTime).toDate() as Date
+//     // );
 
-    // const validExtent = [extent[0] ?? new Date(), extent[1] ?? new Date()] as [
-    //   Date,
-    //   Date
-    // ];
+//     // const validExtent = [extent[0] ?? new Date(), extent[1] ?? new Date()] as [
+//     //   Date,
+//     //   Date
+//     // ];
 
-    // const ys = rangedData.map((elem) => elem.soc);
-    // const xs = rangedData.map((elem) => dayjs(elem.uploadTime).toDate());
+//     // const ys = rangedData.map((elem) => elem.soc);
+//     // const xs = rangedData.map((elem) => dayjs(elem.uploadTime).toDate());
 
-    this.container.attrs = {
-      ...this.container.attrs,
-      html: container,
-      width: 500,
-      height: 400,
-      yAxisLabel: categories[0] ?? '',
-      xAxisText: {
-        rotation: 45,
-        onRender: () => '',
-      },
-      onGetXScale: (chartWidth: number) =>
-        d3
-          .scaleBand()
-          .domain(['labels'])
-          .padding(3)
-          .rangeRound([0, chartWidth]),
-      onGetYScale: (chartHeight: number) =>
-        d3.scaleLinear().domain([0, 100]).rangeRound([chartHeight, 0]),
-    } as d3PlotLib.tContainerAttrs;
+//     this.container.attrs = {
+//       ...this.container.attrs,
+//       html: container,
+//       width: 500,
+//       height: 400,
+//       yAxisLabel: categories[0] ?? '',
+//       xAxisText: {
+//         rotation: 45,
+//         onRender: () => '',
+//       },
+//       onGetXScale: (chartWidth: number) =>
+//         d3
+//           .scaleBand()
+//           .domain(['labels'])
+//           .padding(3)
+//           .rangeRound([0, chartWidth]),
+//       onGetYScale: (chartHeight: number) =>
+//         d3.scaleLinear().domain([0, 100]).rangeRound([chartHeight, 0]),
+//     } as d3PlotLib.tContainerAttrs;
 
-    const plot = this.container.getPlots()[0];
-    plot.attrs = {
-      ...plot.attrs,
-    } as d3PlotLib.tPlotAttrs;
+//     const plot = this.container.getPlots()[0];
+//     plot.attrs = {
+//       ...plot.attrs,
+//     } as d3PlotLib.tPlotAttrs;
 
-    this.container.update();
-  }
-}
+//     this.container.update();
+//   }
+// }
 
-const chart = new Chart();
+// // const chart = new Chart();
 
-useBatteryChart.subscribe((newModel) => {
-  chart.update(newModel);
-});
-
-export { useBatteryChart };
-export type { tPlotDataElem, tBatteryChart, tTimeFrame };
+// // useBatteryChart.subscribe((newModel) => {
+// //   chart.update(newModel);
+// // });
